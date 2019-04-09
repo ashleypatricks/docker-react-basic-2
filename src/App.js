07c4ea7d3529228e7eeb248cba19cb7d676afd36
A <algoash@gmail.com>
2019-04-09 10:03:27 +0200

    Morning update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Whooop Whooop!
+            Azure Pipelines
           </p>
           <a
             className="App-link"
